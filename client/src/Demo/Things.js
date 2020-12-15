43c77fb034bc37b623921aea02e6ff6264cf5d7b
dani7beth <danielledona28@gmail.com>
2020-12-15 10:20:54 -0700

    made sure demo front and back end talk to eachother

--- client/src/Demo/Things.js
+++ client/src/Demo/Things.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react"
+import Axios from 'axios';
+
 export default () =>{
+
+    const[things, setThings] = useState([]);
+
+    useEffect(()=>{
+        getThings();
+    }, []);
+
+    const getThings = async () =>{
+        let res = await Axios.get("api/things");
+        setThings(res.data);
+    }
+
+    const renderThings = () =>{
+       return(
+        things.map((t) => <h2 key={t.id}>{t.name}</h2>)
+       ) 
+    }
+
     return(
-         <div>
+         <>
              <h1>Things</h1>
-         </div>
+             {renderThings()}
+         </>
     ) 
  }
